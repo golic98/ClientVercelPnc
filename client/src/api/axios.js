66874e5ec68a693bd084ed2e9e1f48c5c2e2ec61
golic98 <00214917@uca.edu.sv>
2025-06-22 10:19:38 -0600

    update

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const instance = axios.create({
     baseURL: 'https://service-vercel-pnc.vercel.app/meg',
-    withCredentials: true
+    withCredentials: true,
+    timeout: 10000
 });
 
 export default instance;
