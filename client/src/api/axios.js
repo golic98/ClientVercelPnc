a4b54db268e1f14250d5211eb61e11b9eaed1dd2
golic98 <00214917@uca.edu.sv>
2025-06-22 07:37:45 -0600

    add updates

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "https://service-vercel-pnc.vercel.app/meg",
+    baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true,
     timeout: 10000
 });
