6b5ac0ef5d83a6a7df10a7e15bcc64fb402c0eee
golic98 <00214917@uca.edu.sv>
2025-06-22 18:30:00 -0600

    add updates

--- client/src/pages/admin/AdminUpdate.jsx
+++ client/src/pages/admin/AdminUpdate.jsx
@@ -41,7 +41,6 @@ export default function AdminUpdate() {
         if (params.id) {
             updateProfile(params.id, data);
             navigate("/profileAdmin");
-            handleReload();
         }
     });
 
