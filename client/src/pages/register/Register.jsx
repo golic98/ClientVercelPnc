8b374001bbff453646e74fabcf92479d42a96dce
golic98 <00214917@uca.edu.sv>
2025-06-26 17:22:55 -0600

    update

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -42,7 +42,6 @@ function Register({ onClose }) {
                     role: values.role,
                     password: values.password,
                 };
-
                 await signup(payload);  
             } catch (error) {
                 console.error("Error al crear cuenta:", error);
@@ -53,11 +52,6 @@ function Register({ onClose }) {
     return (
         <div className="register-modal-overlay">
             <div className="register-modal">
-                {registerErrors.map((error, i) => (
-                    <div key={i} className="register-error">
-                        {error}
-                    </div>
-                ))}
                 {mostrarPassword && (
                     <button
                         className="register-back-button"
@@ -154,6 +148,11 @@ function Register({ onClose }) {
                 </form>
                 <p style={{color: "white"}}>¿Ya tienes cuenta? <Link to={"/login"} style={{color: "white"}} className="register-login-link">Inicia sesión</Link> </p>
                 <button style={{padding: "8px"}} onClick={onClose}>Cancelar</button>
+                {registerErrors.map((error, i) => (
+                    <div key={i} className="register-error">
+                        {error}
+                    </div>
+                ))}
             </div>
         </div>
     );
